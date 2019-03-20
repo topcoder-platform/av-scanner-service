4d1aec2035caef97ea0fabfe80a1d992fdc6f810
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2019-03-20 18:57:11 +0530

    STS changes

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-ENV=$1
-CLUSTER=$2
+# ENV=$1
+CLUSTER=$1
 
-ENV_CONFIG=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
+# ENV_CONFIG=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
-AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
-AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
-AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
-AWS_REPOSITORY_CLAMAV=$(eval "echo \$${ENV}_AWS_REPOSITORY_CLAMAV") 
-AUTH0_URL=$(eval "echo \$${ENV}_AUTH0_URL") 
-AUTH0_CLIENT_ID=$(eval "echo \$${ENV}_AUTH0_CLIENT_ID") 
-AUTH0_CLIENT_SECRET=$(eval "echo \$${ENV}_AUTH0_CLIENT_SECRET") 
-AUTH0_AUDIENCE=$(eval "echo \$${ENV}_AUTH0_AUDIENCE") 
-AUTH0_PROXY_SERVER_URL=$(eval "echo \$${ENV}_AUTH0_PROXY_SERVER_URL") 
-BUSAPI_EVENTS_URL=$(eval "echo \$${ENV}_BUSAPI_EVENTS_URL") 
+# AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+# AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
+# AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+# AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
+# AWS_REPOSITORY_CLAMAV=$(eval "echo \$${ENV}_AWS_REPOSITORY_CLAMAV") 
+# AUTH0_URL=$(eval "echo \$${ENV}_AUTH0_URL") 
+# AUTH0_CLIENT_ID=$(eval "echo \$${ENV}_AUTH0_CLIENT_ID") 
+# AUTH0_CLIENT_SECRET=$(eval "echo \$${ENV}_AUTH0_CLIENT_SECRET") 
+# AUTH0_AUDIENCE=$(eval "echo \$${ENV}_AUTH0_AUDIENCE") 
+# AUTH0_PROXY_SERVER_URL=$(eval "echo \$${ENV}_AUTH0_PROXY_SERVER_URL") 
+# BUSAPI_EVENTS_URL=$(eval "echo \$${ENV}_BUSAPI_EVENTS_URL") 
 
 echo "AUTH0_URL=$AUTH0_URL" >api.env
 echo "AUTH0_CLIENT_ID=$AUTH0_CLIENT_ID" >>api.env
@@ -29,16 +29,16 @@ echo "BUSAPI_EVENTS_URL=$BUSAPI_EVENTS_URL" >>api.env
 TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
 CLAMAVTAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY_CLAMAV:$CIRCLE_BUILD_NUM
 
-configure_aws_cli() {
-	aws --version
-	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-	aws configure set default.region $AWS_REGION
-	aws configure set default.output json
-	echo "Configured AWS CLI."
-}
+# configure_aws_cli() {
+# 	aws --version
+# 	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+# 	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+# 	aws configure set default.region $AWS_REGION
+# 	aws configure set default.output json
+# 	echo "Configured AWS CLI."
+# }
 
-configure_aws_cli
+# configure_aws_cli
 sed -i='' "s|app:latest|$TAG|" docker-compose.yml
 sed -i='' "s|clamav:latest|$CLAMAVTAG|" docker-compose.yml
 docker-compose build
