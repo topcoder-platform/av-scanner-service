d886f21f43b91d5de7701da207f87e951334f2ee
CWD <cwd@cloudspokes.com>
2021-06-03 13:41:55 +0000

    added logging

--- src/controllers/ClamAVController.js
+++ src/controllers/ClamAVController.js
@@ -64,19 +64,23 @@ function scan (req, res, next) {
    * @param {Function} next the next middleware
    */
 function batchScan (req, res, next) {
+  logger.debug('ClamAVController->batchScan(): start')
   const schema = joi.object().keys({
     submissionId: joi.string().uuid().required(),
     url: joi.string().uri().trim().required(),
     fileName: joi.string().required()
   }).required()
 
+  logger.debug('ClamAVController->batchScan(): validating body')
   const result = joi.validate(req.body, schema)
   if (result.error) {
+    logger.error(`ClamAVController->batchScan(): validationg error ${result.error}`)
     result.error.status = 400
     return next(result.error)
   }
 
   // Request body for Posting to Bus API
+  logger.debug('ClamAVController->batchScan(): building unscanned event')
   const reqBody = {
     'topic': config.AVSCAN_TOPIC,
     'originator': originator,
@@ -85,6 +89,7 @@ function batchScan (req, res, next) {
     'payload': _.extend({ 'status': 'unscanned' }, req.body)
   }
 
+  logger.debug(`ClamAVController->batchScan(): posting unscanned event to the event bus: ${JSON.stringify(reqBody)}`)
   helper.getM2Mtoken().then((token) => {
     request
       .post(config.BUSAPI_EVENTS_URL)
@@ -93,8 +98,10 @@ function batchScan (req, res, next) {
       .send(reqBody)
       .then(result => {
         res.status(result.statusCode).json({})
+        logger.debug('ClamAVController->batchScan(): completed')
       })
       .catch(err => {
+        logger.error(`ClamAVController->batchScan(): validationg error ${err}`)
         return next(err)
       })
   })
